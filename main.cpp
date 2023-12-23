0642d7926c381a3214f2fb2a54c87f57cf8b764b
FFrank <1850432206@qq.com>
2023-12-24 02:30:31 +0800

    引入了知乎上的利用遗传算法解决TSP的代码。

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <Fl/Fl.H>
+#include <Fl/Fl_Window.H>
+#include <Fl/Fl_Box.H>
+#include <Fl/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Text_Display.H>
+#include <FL/Fl_Text_Buffer.H>
+#include <FL/Fl_Text_Editor.H>
+#include <FL/Fl_Text_Buffer.H>
+
+//可能会用到boost库
+
+
+
+using namespace std;
+
+
+
+int main(){
+
+}
+
+
---
+++ makefile
@@ -0,0 +1,2 @@
+main:main.cpp
+	g++ -o main main.cpp -std=c++11 -O3 -lfltk -lfltk_images
